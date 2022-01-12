ea352958798879d0040ccce7448bfebd05c26f8b
salam <es_belhadj@esi.dz>
2022-01-12 16:56:14 +0100

    impliment the list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasklist = [{
+  description: 'task 1',
+  completed: true,
+  index: 1,
+}, {
+  description: 'task 2',
+  completed: false,
+  index: 2,
+}, {
+  description: 'task 3',
+  completed: true,
+  index: 3,
+}, {
+  description: 'task 4',
+  completed: true,
+  index: 4,
+}];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'Selma'], ' ');
-  element.classList.add('hello');
+const render = (tasks) => {
+  const tasksListSorted = tasks.sort((a, b) => a.index - b.index);
+  const tasksContainer = document.querySelector('.tasks-container');
+  let todosHtml = '';
+  tasksListSorted.forEach((todo) => {
+    todosHtml += ` <div class="todo-item">
+        <input type="checkbox" class="checkmark" /><span> ${todo.description}</span>
+    </div>`;
+  });
+  tasksContainer.innerHTML = todosHtml;
+};
 
-  return element;
-}
-
-document.body.appendChild(component());
+render(tasklist);
