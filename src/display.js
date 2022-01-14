4920501b8a55a25eb528e33d2210ed398c8e5d72
salam <es_belhadj@esi.dz>
2022-01-14 22:38:33 +0100

    implement an interactive to-do list

--- src/display.js
+++ src/display.js
@@ -3,12 +3,14 @@ const display = (taskLists, tasksContainer) => {
   tasksContainer.innerHTML = '';
   let todosHtml = '';
   sortedTodos.forEach((todo) => {
+    const checkedTodo = todo.completed ? 'checked' : '';
+    const checkClass = todo.completed ? 'checked' : '';
     todosHtml += `  <div class="todo-item">
                               <div>
-                                  <input id="${todo.id}" type="checkbox" class="todo-check" />
-                                  <input id="${todo.index}" type="text" class="todo-edit" value="${todo.description}" />
+                                  <input id="${todo.id}" type="checkbox" class="todo-check" ${checkedTodo} />
+                                  <input id="${todo.id}" type="text" class="todo-edit" ${checkClass} value="${todo.description}" />
                               </div>
-                              <button id="${todo.index}" class="remove-btn"> <i class="fas fa-trash"></i></button>
+                              <button id="${todo.id}" class="remove-btn"> <i class="fas fa-trash"></i></button>
                           </div>
           `;
   });
@@ -20,8 +22,8 @@ const display = (taskLists, tasksContainer) => {
   removeBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const element = btn.parentNode;
-      taskLists.RemoveTask(Number(e.target.parentNode.id));
       element.remove();
+      taskLists.RemoveTask(e.target.parentNode.id);
     });
   });
 
@@ -29,7 +31,17 @@ const display = (taskLists, tasksContainer) => {
   const todosContent = document.querySelectorAll('.todo-edit');
   todosContent.forEach((todo) => {
     todo.addEventListener('change', (e) => {
-      taskLists.EditTask(Number(e.target.id), e.target.value);
+      taskLists.EditTask(e.target.id, e.target.value);
+    });
+  });
+
+  // Complete Todo
+  const taskCheck = document.querySelectorAll('.todo-check');
+  taskCheck.forEach((todo) => {
+    todo.addEventListener('change', (e) => {
+      const { id } = e.target;
+      taskLists.CompleteTask(id, e.target.checked);
+      e.target.parentNode.lastElementChild.classList.toggle('checked');
     });
   });
 };
